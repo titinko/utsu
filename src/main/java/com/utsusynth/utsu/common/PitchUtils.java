cafc508db29c16cc2913f5493dd974c39d82fdb4
Steve <dude@dork.city>
2017-10-29 22:20:23 -0700

    Add comments to pitch utils

--- src/main/java/com/utsusynth/utsu/common/PitchUtils.java
+++ src/main/java/com/utsusynth/utsu/common/PitchUtils.java
@@ -10,20 +10,32 @@ public class PitchUtils {
 	private PitchUtils() {
 	}
 
+	/**
+	 * Convert a note num (where 24 = C1) to the string description of what pitch it represents.
+	 */
 	public static String noteNumToPitch(int noteNum) {
 		return PITCHES.get(noteNum % 12) + Integer.toString(noteNum / 12 - 1);
 	}
 
+	/**
+	 * Convert a pitch string into a note number (where 24 = C1).
+	 */
 	public static int pitchToNoteNum(String fullPitch) {
 		int octave = Integer.parseInt(fullPitch.substring(fullPitch.length() - 1));
 		int pitchNum = PITCHES.indexOf(fullPitch.substring(0, fullPitch.length() - 1));
 		return (octave + 1) * 12 + pitchNum;
 	}
 
+	/**
+	 * Convert the row number in the UI (where the top is 0) into a pitch string.
+	 */
 	public static String rowNumToPitch(int rowNum) {
 		return REVERSE_PITCHES.get(rowNum % 12) + Integer.toString(7 - (rowNum / 12));
 	}
 
+	/**
+	 * Convert a pitch string into the row number in the UI (where the top is 0).
+	 */
 	public static int pitchToRowNum(String fullPitch) {
 		int octave = Integer.parseInt(fullPitch.substring(fullPitch.length() - 1));
 		int pitchNum = REVERSE_PITCHES.indexOf(fullPitch.substring(0, fullPitch.length() - 1));
