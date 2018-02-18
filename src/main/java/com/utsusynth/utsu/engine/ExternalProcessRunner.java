f2b247198261d3a2acaf6772ec8877d3db004469
Emma Breen <emma.ebreen@gmail.com>
2018-02-17 17:33:20 -0800

    Destroy any ongoing processes when Utsu closes.

--- src/main/java/com/utsusynth/utsu/engine/ExternalProcessRunner.java
+++ src/main/java/com/utsusynth/utsu/engine/ExternalProcessRunner.java
@@ -1,42 +1,53 @@
 package com.utsusynth.utsu.engine;
 
-import com.utsusynth.utsu.common.exception.ErrorLogger;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import com.utsusynth.utsu.common.exception.ErrorLogger;
 
 /**
  * Class that runs an external command-line process with the provided arguments.
  */
 public class ExternalProcessRunner {
-	private static final ErrorLogger errorLogger = ErrorLogger.getLogger();
+    private static final ErrorLogger errorLogger = ErrorLogger.getLogger();
 
-	void runProcess(String... args) {
-		ProcessBuilder builder = new ProcessBuilder(args);
-		builder.redirectErrorStream(true);
-		try {
-			Process process = builder.start();
-			watch(process);
-			process.waitFor();
-		} catch (IOException | InterruptedException e) {
-			errorLogger.logError(e);
-		}
-	}
+    private Process curProcess;
 
-	private void watch(final Process process) {
-		new Thread() {
-			public void run() {
-				BufferedReader input = new BufferedReader(
-						new InputStreamReader(process.getInputStream()));
-				String line = null;
-				try {
-					while ((line = input.readLine()) != null) {
-						System.out.println(line);
-					}
-				} catch (IOException e) {
-					errorLogger.logError(e);
-				}
-			}
-		}.start();
-	}
+    public ExternalProcessRunner() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            // Destroy any ongoing processes when Utsu closes.
+            if (curProcess != null && curProcess.isAlive()) {
+                curProcess.destroy();
+            }
+        }));
+    }
+
+    void runProcess(String... args) {
+        ProcessBuilder builder = new ProcessBuilder(args);
+        builder.redirectErrorStream(true);
+        try {
+            curProcess = builder.start();
+            watch(curProcess.getInputStream());
+            curProcess.waitFor();
+        } catch (IOException | InterruptedException e) {
+            errorLogger.logError(e);
+        }
+    }
+
+    private void watch(final InputStream inputStream) {
+        new Thread() {
+            public void run() {
+                BufferedReader input = new BufferedReader(new InputStreamReader(inputStream));
+                String line = null;
+                try {
+                    while ((line = input.readLine()) != null) {
+                        System.out.println(line);
+                    }
+                } catch (IOException e) {
+                    errorLogger.logError(e);
+                }
+            }
+        }.start();
+    }
 }
