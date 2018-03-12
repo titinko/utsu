7721dfda2817dff827524b30f415ef82ee6dcba7
Emma Breen <emma.ebreen@gmail.com>
2018-03-11 23:37:26 -0700

    Added chinese (trad) and marked as TW pending info from translator

--- src/main/java/com/utsusynth/utsu/UtsuModule.java
+++ src/main/java/com/utsusynth/utsu/UtsuModule.java
@@ -42,8 +42,9 @@ public class UtsuModule extends AbstractModule {
         ImmutableList<NativeLocale> allLocales = ImmutableList.of(
                 defaultLocale,
                 new NativeLocale(new Locale("ja")),
-                new NativeLocale(new Locale("zh")),
-                new NativeLocale(new Locale("es")));
+                new NativeLocale(new Locale("es")),
+                new NativeLocale(new Locale("zh", "CN")),
+                new NativeLocale(new Locale("zh", "TW")));
         return new Localizer(defaultLocale, allLocales);
     }
 
