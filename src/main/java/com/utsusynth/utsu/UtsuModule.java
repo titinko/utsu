34c580257e81b7c0ab6be4534018f2699ff00c72
Emma Breen <emma.ebreen@gmail.com>
2018-08-27 01:25:59 -0700

    Changes language code for Indonesian to in.

--- src/main/java/com/utsusynth/utsu/UtsuModule.java
+++ src/main/java/com/utsusynth/utsu/UtsuModule.java
@@ -44,7 +44,7 @@ public class UtsuModule extends AbstractModule {
                 new NativeLocale(new Locale("ja")),
                 new NativeLocale(new Locale("es")),
                 new NativeLocale(new Locale("it")),
-                new NativeLocale(new Locale("id")),
+                new NativeLocale(new Locale("in")),
                 new NativeLocale(new Locale("zh", "CN")),
                 new NativeLocale(new Locale("zh", "TW")));
         return new Localizer(defaultLocale, allLocales);
