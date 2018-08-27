63c35588eb4a28e9001481d9b8ce2a0cefb1068c
Emma Breen <emma.ebreen@gmail.com>
2018-08-27 01:21:23 -0700

    Adds placeholder messages for all languages.

--- src/main/java/com/utsusynth/utsu/common/i18n/Localizer.java
+++ src/main/java/com/utsusynth/utsu/common/i18n/Localizer.java
@@ -3,44 +3,44 @@ package com.utsusynth.utsu.common.i18n;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ResourceBundle;
-
 import com.google.common.collect.ImmutableList;
 
 public class Localizer {
-	private final ImmutableList<NativeLocale> supportedLocales;
-	private final LinkedList<Localizable> targets;
-	private NativeLocale locale;
-	private ResourceBundle bundle;
+    private final ImmutableList<NativeLocale> supportedLocales;
+    private final LinkedList<Localizable> targets;
+    private NativeLocale locale;
+    private ResourceBundle bundle;
 
-	public Localizer(NativeLocale locale, List<NativeLocale> supportedLocales) {
-		this.supportedLocales = ImmutableList.copyOf(supportedLocales);
-		this.targets = new LinkedList<>();
-		this.locale = locale;
-		this.bundle = ResourceBundle.getBundle("messages.messages", locale.getLocale());
-	}
+    public Localizer(NativeLocale locale, List<NativeLocale> supportedLocales) {
+        this.supportedLocales = ImmutableList.copyOf(supportedLocales);
+        this.targets = new LinkedList<>();
+        this.locale = locale;
+        this.bundle = ResourceBundle.getBundle("messages.messages", locale.getLocale());
+    }
 
-	public void localize(Localizable target) {
-		if (targets.contains(target)) {
-			// TODO: Handle this, or just ignore it. I'm not a cop.
-			return;
-		}
-		targets.add(target);
-		target.localize(this.bundle);
-	}
+    public void localize(Localizable target) {
+        if (targets.contains(target)) {
+            // TODO: Handle this, or just ignore it. I'm not a cop.
+            return;
+        }
+        targets.add(target);
+        target.localize(this.bundle);
+    }
 
-	public void setLocale(NativeLocale locale) {
-		this.locale = locale;
-		this.bundle = ResourceBundle.getBundle("messages.messages", locale.getLocale());
-		for (Localizable target : targets) {
-			target.localize(this.bundle);
-		}
-	}
+    public void setLocale(NativeLocale locale) {
+        this.locale = locale;
+        this.bundle = ResourceBundle.getBundle("messages.messages", locale.getLocale());
+        System.out.println(locale.getLocale() + " " + bundle.getLocale());
+        for (Localizable target : targets) {
+            target.localize(this.bundle);
+        }
+    }
 
-	public NativeLocale getCurrentLocale() {
-		return locale;
-	}
+    public NativeLocale getCurrentLocale() {
+        return locale;
+    }
 
-	public List<NativeLocale> getAllLocales() {
-		return supportedLocales;
-	}
+    public List<NativeLocale> getAllLocales() {
+        return supportedLocales;
+    }
 }
