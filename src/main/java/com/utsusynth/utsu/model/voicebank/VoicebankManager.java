5383d168b2513eba62e0192328550b156f8919d4
Simon Williams <mooncat69mail@gmail.com>
2020-04-09 21:21:28 +0100

    ES-6 Finding and parsing all voice banks on startup

--- src/main/java/com/utsusynth/utsu/model/voicebank/VoicebankManager.java
+++ src/main/java/com/utsusynth/utsu/model/voicebank/VoicebankManager.java
@@ -2,9 +2,14 @@ package com.utsusynth.utsu.model.voicebank;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import com.google.inject.Inject;
 import com.utsusynth.utsu.common.exception.ErrorLogger;
+import com.utsusynth.utsu.files.VoicebankFileManager;
+import com.utsusynth.utsu.files.VoicebankReader;
 
 /**
  * Manages all voicebanks in use by Utsu. This class is a singleton to ensure the same voicebank
@@ -12,11 +17,22 @@ import com.utsusynth.utsu.common.exception.ErrorLogger;
  */
 public class VoicebankManager {
     private static final ErrorLogger errorLogger = ErrorLogger.getLogger();
+    public static final String DEFAULT_VOICE_PATH = "assets/voice";
 
     private final Map<File, Voicebank> voicebanks;
 
-    public VoicebankManager() {
+    @Inject
+    public VoicebankManager(VoicebankReader voicebankReader) {
         voicebanks = new HashMap<>();
+        loadDefaultBanks(voicebankReader);
+    }
+
+    private void loadDefaultBanks(VoicebankReader voicebankReader) {
+        ArrayList<File> dirs = new VoicebankFileManager().getVoiceBankDirs(new File(DEFAULT_VOICE_PATH));
+        dirs.forEach(d -> {
+            Voicebank bank = voicebankReader.loadVoicebankFromDirectory(d);
+            setVoicebank(normalize(d), bank);
+        });
     }
 
     public boolean hasVoicebank(File location) {
