bc5541a80d9913d12ad257310c1a988fcb77832e
Emma Breen <emma.ebreen@gmail.com>
2020-05-25 15:42:15 -0700

    Set up test module.

---
+++ src/test/java/module-info.java
@@ -0,0 +1,9 @@
+module com.utsusynth.utsu {
+    requires com.google.common;
+
+    exports com.utsusynth.utsu;
+
+    // Extra test modules.
+    requires jakarta.activation;
+    requires junit;
+}
